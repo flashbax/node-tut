0bdf4d66fd4a83e984644cbb72d758607e423174
flashbax <jkiwtco@gmail.com>
2017-02-08 14:10:10 -0500

    hello_http.js and main.js file

--- hello_http.js
+++ hello_http.js
@@ -1,7 +1,7 @@
-var http = require('http'); 
+var http = require('http'); //module into var
 
-var server = http.createServer(function(req, res) { 
+var server = http.createServer(function(req, res) { //The argument passed into this call is a closure that is called whenever an http request comes in.
     res.writeHead(200)
     res.end('Hello HTTP');
 });
-server.listen(8080);
+server.listen(8080); //tell node.js the port on which we want our server to run
---
+++ main.js
@@ -0,0 +1,5 @@
+
+var hello = require('./hello'); 
+//the require('./hello') is used to import the contents from another JavaScript file. 
+//'./' indicates that the file is located in the same directory as 'main.js'
+hello.world();
